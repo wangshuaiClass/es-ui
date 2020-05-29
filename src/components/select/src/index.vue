1b0dcea39218bce9bb30bf82245b53551c9774ee
lqinkun <lqinkun@linewell.com>
2020-05-29 17:47:40 +0800

    lqk

---
+++ src/components/select/src/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="es-select">
+    <input type="text" placeholder="请选择">
+  </div>
+</template>
+<script>
+const Popper = isServer ? function() {} : require('popper.js/dist/umd/popper.js');  // eslint-disable-line
+
+export default {
+  
+}
+</script>
